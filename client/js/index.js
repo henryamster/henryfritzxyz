58b7eb43eaae5ecd70cadd5aa407c867a3a69296
henry fritz <henryamsterfritz@gmail.com>
2017-04-01 03:55:56 +0000

    Version 2

--- client/js/index.js
+++ client/js/index.js
@@ -1,94 +1,34 @@
-function Mover(m, x, y) {
-  this.loc = createVector(x, y)
-  this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0);
-  this.mass = m * 3;
-
-  this.update = function() {
-    this.vel.add(this.acc);
-    this.loc.add(this.vel);
-    this.vel.limit(5);
-    this.acc.mult(.008);
-  }
-
-  this.display = function() {
-    //stroke(0);
-    //strokeWeight(12);
-    var date = new Date();
-   if (date.getDay() == 5)
-    {colorMode(HSB);
-     
-    fill(random(0,299),255,255);
-    noStroke();
-    }
-    var p = random(0.99, 1.01)
-     ellipse(this.loc.y, this.loc.x, this.mass  * 1.5, this.mass * 1.5);
-    ellipse(this.loc.x, this.loc.y, this.mass  * p, this.mass * p);
-
-  }
-  this.applyForce = function(force) {
-    var f = p5.Vector.lerp(force, this.mass);
-    this.acc.sub(f);
-    this.acc.dot(f);
-  }
-  this.checkEdges = function() {
-    if (this.loc.x > width) {
-      this.loc.x = width;
-      this.vel.x *= -1;
-    } else if (this.loc.x < 0) {
-      this.loc.x = 0;
-      this.vel.x *= -1;
-
-    }
-    if (this.loc.y > height) {
-      this.loc.y = height;
-      this.vel.y *= -1;
-
-    } else if (this.loc.y < 0) {
-      this.loc.x = 0;
-      this.vel.x *= -1;
-
-    }
-  };
-}
-var movers = [];
-var can;
-
+window.addEventListener("resize", setup);
 function setup() {
-  var can = createCanvas(windowWidth, windowHeight);
-  background(0);
-  pixelDensity(1);
-  for (var i = 0; i < 2000; i++) {
-    movers[i] = new Mover(random(.5, 8), random(0, width * 2), random(0, height * 2));
-
-  }
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  background(60, 10, 100);
 }
 
 function draw() {
-  //background(0);
-  //noStroke();
-  translate(-cos(PI * frameRate % 5));
-  for (var i = 0; i < movers.length; i++) {
-    rotate(-sin(PI * movers[i].mass ^ 2));
-    var wind = createVector(0.05 * movers[i].mass / frameCount, -.002 * sqrt(movers[i].mass));
-    var gravity = createVector(-3.6, -88.1 * movers[i].mass ^ 2);
+  translate(width * .5, height * .5);
 
-    movers[i].applyForce(wind);
-    movers[i].applyForce(wind);
-    movers[i].update();
-    movers[i].display();
+  scale(sin(frameCount) * .20);
+  rotate(frameCount % 360) * PI / 60;
 
-    if (frameCount < 300) {
-      movers[i].checkEdges();
-
+  var x = random(0, windowWidth);
+  var y = random(0, windowHeight);
+  var t = frameCount % x;
+  var movey = atan2(cos(y), min(8.79, x * 2.72)) * max(9.91 + y, -cos(x + 5.50)) * cos(x) * cos(t * y) - max(1.35, y / 3) + sin(t * x) * atan2(y, x) + .28 + t * floor(sqrt(.0042));
+  var proj = random(25, 195);
+  for (var i = 5; i < proj; i++) {
+    fill((25 * movey / proj + i) + 180 % 255, 140, 160);
+    stroke((25 * movey / proj + i) + 180 % 255, 140, 120);
+    for (var p = 0; p < 17; p++) {
+      shootingSquare(x + i, y + i, i, movey, p % 3);
     }
-    /*if (frameCount === 1200) {
-      var booly = true
-    }
-    if (booly === true) {
-      saveCanvas(can, 'rtxx', 'jpg');
-      booly === false;
-    }*/
+
   }
+
+}
+
+function shootingSquare(x, y, i, movey, factor) {
+
+  ellipse(x + i * 3 + y, y + i * 3 - x, movey + i, movey + i);
 
 }
