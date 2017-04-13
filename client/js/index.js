005d6f695f65eee71ddedbaedee84211fdae3571
henry fritz <henryamsterfritz@gmail.com>
2017-04-13 20:02:41 +0000

    CIS 240 project

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,18 @@
 window.addEventListener("resize", setup);
+var minute = new Date();
+var minutes = minute.getMinutes();
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
-  background(60, 10, 100);
+  background(160, 45, 200);
+
+  
 }
 
 function draw() {
   translate(width * .5, height * .5);
 
-  scale(sin(frameCount) * .20);
+  scale(sin(frameCount) * .30);
   rotate(frameCount % 360) * PI / 60;
 
   var x = random(0, windowWidth);
@@ -17,8 +21,8 @@ function draw() {
   var movey = atan2(cos(y), min(8.79, x * 2.72)) * max(9.91 + y, -cos(x + 5.50)) * cos(x) * cos(t * y) - max(1.35, y / 3) + sin(t * x) * atan2(y, x) + .28 + t * floor(sqrt(.0042));
   var proj = random(25, 195);
   for (var i = 5; i < proj; i++) {
-    fill((25 * movey / proj + i) + 180 % 255, 140, 160);
-    stroke((25 * movey / proj + i) + 180 % 255, 140, 120);
+    fill((25 * movey / proj + i) + 180 % 255, 40, 160);
+    stroke((25 * movey / proj + i) + 180 % 255, 40, 120);
     for (var p = 0; p < 17; p++) {
       shootingSquare(x + i, y + i, i, movey, p % 3);
     }
@@ -28,7 +32,11 @@ function draw() {
 }
 
 function shootingSquare(x, y, i, movey, factor) {
-
+  
+if (minutes % 2 == 0){
   ellipse(x + i * 3 + y, y + i * 3 - x, movey + i, movey + i);
-
+}
+else {
+   rect(x + i * 3 + y, y + i * 3 - x, movey + i, movey + i);
+}
 }
--- server.js
+++ server.js
@@ -4,4 +4,7 @@ app.use(express.static('client'));
 app.get('/', function (req, res){
     res.sendfile(__dirname + '/client/index.html');
 });
+app.get('/CIS240', function (req, res){
+    res.sendFile(__dirname + '/240/cis240.html');
+})
 app.listen(process.env.PORT, process.env.IP);
