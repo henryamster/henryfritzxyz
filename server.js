d041c94679e4cdf074a22812c2b1c00d306266b1
henry fritz <henryamsterfritz@gmail.com>
2017-05-18 21:25:15 +0000

    added link back to blog from post page

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var app = express();
-app.use(express.static('client'));
-app.get('/', function (req, res){
-    res.sendfile(__dirname + '/client/index.html');
-});
-app.get('/blog', function (req, res){
-    res.sendFile(__dirname + '/client/blog.html');
-})
-app.get('/post', function (req, res){
-    res.sendFile(__dirname + '/client/post.html');
-})
 
-console.log(process.env.IP + ":" + process.env.PORT)
-app.listen(process.env.PORT, process.env.IP);
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+router.use(express.static(path.resolve(__dirname, 'client')));
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("blog server be live at", addr.address + ":" + addr.port);
+});
